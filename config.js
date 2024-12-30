a1b1213f3d4f418730f9681774da2b8400cd6efa
kasunking <162451315+kasunking@users.noreply.github.com>
2024-12-30 13:09:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "G2pUmarZ#nB0dBHUCE9ltvpbVDMsJ7VRd4nmClJ3VJ1qhcAeQWjI", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
